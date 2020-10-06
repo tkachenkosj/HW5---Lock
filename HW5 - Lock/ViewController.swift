0debec9646e8b6c827608e91ab7737dcca1dbfd9
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-06 13:41:36 +0300

    Initial Commit

---
+++ HW5 - Lock/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5 - Lock
+//
+//  Created by Сергей Ткаченко on 06.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
