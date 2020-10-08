a34228ca06effedecbd9bc79f75cbaf31afedca6
Sergey Tkachenko <tkachenkosj@gmail.com>
2020-10-08 12:52:49 +0300

    Добавлен код для перемещения по текстовым полям и активации кнопки done как login - пока не работает

--- HW5 - Lock/Controller/LogInViewController.swift
+++ HW5 - Lock/Controller/LogInViewController.swift
@@ -78,9 +78,17 @@ extension LogInViewController: UITextFieldDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
-        
         view.endEditing(true)
     }
     
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            textField.resignFirstResponder()
+            passwordTextField.becomeFirstResponder()
+        } else {
+            pressedLogInButton()
+        }
+        return true
+    }
 }
 
